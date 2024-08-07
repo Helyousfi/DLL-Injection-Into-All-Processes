1123b74dfdb25eb44727fc4995d99f0ec875c5ec
helyousfi <visioncomputer200@gmail.com>
2024-08-07 23:53:50 +0200

    Updated Sys

--- InjectAll/Drv/DrvMain.cpp
+++ InjectAll/Drv/DrvMain.cpp
@@ -6,7 +6,7 @@ void NTAPI DriverUnload(PDRIVER_OBJECT DriverObject)
 {
 	// Routine that is called when driver is unloaded
 	NTSTATUS status = 0;
-	DbgPrintLine("DiverUnLoad(0x%p), status=0x%x", DriverObject, status);
+	DbgPrintLine("DiverUnload(0x%p), status=0x%x", DriverObject, status);
 }
 
 extern "C" NTSTATUS NTAPI DriverEntry(
@@ -19,7 +19,7 @@ extern "C" NTSTATUS NTAPI DriverEntry(
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 	// Debugging output function
-	DbgPrintLine("DiverLoad(0x%p, %wZ)" , DriverObject, RegistryPath);
+	DbgPrintLine("DiverLoad(0x%p, %wZ)", DriverObject, RegistryPath);
 	DriverObject->DriverUnload = DriverUnload;
 
 	return 0;
--- InjectAll/Drv/SharedDefs.h
+++ InjectAll/Drv/SharedDefs.h
@@ -1,4 +1,4 @@
 // Definitiions that are shared across projects in this solution
 #pragma once
 
-#define DBG_PREFIX_ALL "InjectAll_"
+#define DBG_PREFIX_ALL "InjectAll_"		// Prefix to be added in all DBGPrint
