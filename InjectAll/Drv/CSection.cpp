d09b4451be3b946cad1fda6f22682085f02e4730
Helyousfi <visioncomputer200@gmail.com>
2024-08-09 10:54:50 +0200

    Update CSection.cpp

--- InjectAll/Drv/CSection.cpp
+++ InjectAll/Drv/CSection.cpp
@@ -2,19 +2,6 @@
 
 //  
 //    Test solution that demonstrates DLL injection into all running processes
-//    Copyright (c) 2021 www.dennisbabkin.com
-//
-//        https://dennisbabkin.com/blog/?i=AAA10800
-//
-//    Credit: Rbmm
-//
-//        https://github.com/rbmm/INJECT
-//
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//    
-//        https://www.apache.org/licenses/LICENSE-2.0
 //    
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
